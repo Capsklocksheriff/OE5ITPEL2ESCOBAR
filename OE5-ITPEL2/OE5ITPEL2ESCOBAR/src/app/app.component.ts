739763911ed760dd9e82a521aec5493a0d631311
Capsklocksheriff <marvinandrei88@gmail.com>
2021-11-05 15:59:22 +0800

    First commit

---
+++ OE5-ITPEL2/OE5ITPEL2ESCOBAR/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  public appPages = [
+    { title: 'Home', url: '/folder/Home', icon: 'home' },
+    { title: 'Chat', url: '/folder/Chat', icon: 'chatbox' },
+    { title: 'Contacts', url: '/folder/Contacts', icon: 'reader' },
+   // { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
+    //{ title: 'Trash', url: '/folder/Trash', icon: 'trash' },
+    //{ title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+  ];
+  public labels = ['Family', 'Friends', 'Notes'];
+  constructor() {}
+}
